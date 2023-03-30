f61a7842ae8d623fd630e54874cec997a3d65263
Ali Can <ainanc@gmail.com>
2023-03-29 21:46:14 -0400

    US06 completed

--- src/test/java/com/library/steps/Hooks.java
+++ src/test/java/com/library/steps/Hooks.java
@@ -1,6 +1,7 @@
 package com.library.steps;
 
 import com.library.utility.ConfigurationReader;
+import com.library.utility.DB_Util;
 import com.library.utility.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -12,8 +13,8 @@ import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
-    @Before
-    public void setUp(){
+    @Before("@ui")
+    public void setUp() {
         System.out.println("this is coming from BEFORE");
         Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         Driver.getDriver().manage().window().maximize();
@@ -22,20 +23,32 @@ public class Hooks {
 
     }
 
-    @After
-    public void tearDown(Scenario scenario){
+    @After("@ui")
+    public void tearDown(Scenario scenario) {
         System.out.println("this is coming from AFTER");
 
-        if(scenario.isFailed()){
+        if (scenario.isFailed()) {
             final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot,"image/png","screenshot");
+            scenario.attach(screenshot, "image/png", "screenshot");
         }
 
         Driver.closeDriver();
 
     }
 
+    @Before("@db")
+    public void setUpDB() {
+        System.out.println("DB connection is DONE ");
 
+        DB_Util.createConnection();
 
+    }
 
+    @After("@db")
+    public void destroyDB() {
+        System.out.println("DB connection is closed ");
+
+        DB_Util.destroy();
+
+    }
 }
---
+++ src/test/resources/library.sql
@@ -0,0 +1,10 @@
+select count(id) from books;
+
+select count(distinct id)from users;
+
+select name,author,isbn from books
+where name ='Ali Can in Wonderland';
+
+
+
+
